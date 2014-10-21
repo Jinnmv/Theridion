4b925ce413892cfbf3a5f88ae36b71e090c691aa
Anton S <jin.wishmaster@gmail.com>
2014-10-21 14:25:47 +0300

    Update timer.go

--- timer.go
+++ timer.go
@@ -9,3 +9,10 @@ func timeTrack(start time.Time, name string) {
     elapsed := time.Since(start)
     log.Printf("%s took %ss", name, elapsed.Seconds())
 }
+
+// example:
+/*func factorial(n *big.Int) (result *big.Int) {
+    defer timeTrack(time.Now(), "factorial")
+    // ... do some things, maybe even return under some condition
+    return n
+}*/
